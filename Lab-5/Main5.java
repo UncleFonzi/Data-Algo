1300bb469a52b89835f959441c6d18895cb4ffd7
UncleFonzi <dpwalker56@gmail.com>
2025-11-23 23:13:02 -0500

    Lab 6

--- Lab-5/Main5.java
+++ Lab-5/Main5.java
@@ -2,7 +2,7 @@ class Main5 {
   public static void main(String[] args) {
 
     // Step 1 - Create a BST tree object called lab5Tree
-    BST lab5Tree = new BST();
+    BST<Integer> lab5Tree = new BST<>();
 
     // Step 2 - Insert the following values: 13, 22, 36, 5, 48, 17, 39, 2, 26, 40, 29, 34, 10
     int[] values = {13, 22, 36, 5, 48, 17, 39, 2, 26, 40, 29, 34, 10};
@@ -19,7 +19,6 @@ class Main5 {
     System.out.print("\nPostorder: ");
     lab5Tree.postorder();
 
-
     // Step 6 - Traverse and output the values using preorder
     System.out.print("\nPreorder: ");
     lab5Tree.preorder();
@@ -32,10 +31,9 @@ class Main5 {
     System.out.println("Search 37: " + lab5Tree.search(37));
 
     // Step 9 - Using the path() method, display the path from the root to 2
-    System.out.println("Path to 2: " + lab5Tree.path(2));
-
+    lab5Tree.path(2).forEach(node -> System.out.print(node.element + " "));
+    
     // Step 10 - Display the path from the root to 34
-    System.out.println("Path to 34: " + lab5Tree.path(34));
-
+    lab5Tree.path(34).forEach(node -> System.out.print(node.element + " ")); 
   }
 }
